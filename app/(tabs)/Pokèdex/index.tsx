51c1e0b99f8f43bfae00643066037a0c2a5c079f
tomm00se <tom@moose17.dev>
2024-09-12 14:47:35 +0100

    update index to get renderitem via ListRenderItemInfo and displays pokemon tile component via flatlist

--- app/(tabs)/Pokèdex/index.tsx
+++ app/(tabs)/Pokèdex/index.tsx
@@ -1,23 +1,62 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  StyleSheet,
+  ListRenderItemInfo,
+  FlatList,
+  SafeAreaView,
+} from "react-native";
+import React from "react";
+import {
+  PokemonTile,
+  PokemonTileProps,
+} from "@/components/Pokèdex/PokemonTile";
 
-const list = () => {
+import pokemonData from "@/utils/pokemonData";
+
+const Pokèdex = () => {
+  const renderItem = ({ item }: ListRenderItemInfo<PokemonTileProps>) => (
+    <PokemonTile {...item} />
+  );
   return (
-    <View style={Styles.container}>
-      <Text style={Styles.text}>Pokèdex</Text>
-    </View>
-  )
-}
+    <SafeAreaView style={styles.safeArea}>
+      <View>
+        <Text style={styles.title}>Pokèdex</Text>
+        <FlatList
+          data={pokemonData}
+          renderItem={renderItem}
+          numColumns={2}
+          keyExtractor={(item) => item.id}
+          contentContainerStyle={styles.contentContainerStyle}
+          columnWrapperStyle={styles.columnWrapperStyle}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
 
-export default list
+export default Pokèdex;
 
-const Styles = StyleSheet.create({
-  container:{
-    marginVertical: 30 
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
   },
-  text:{
+  title: {
     fontSize: 32,
-    fontWeight: 'bold',
-    padding: 30
-  }
-})
+    fontWeight: "bold",
+    padding: 20,
+  },
+  link: {
+    fontSize: 16,
+    paddingHorizontal: 20,
+  },
+  contentContainerStyle: {
+    justifyContent: "space-around",
+    padding: 20,
+  },
+  columnWrapperStyle: {
+    display: "flex",
+    marginBottom: 10,
+    gap: 10,
+  },
+});
