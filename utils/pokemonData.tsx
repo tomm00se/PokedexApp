24b900074d2d5d1f3c580d5cb6dbcda91f86c48f
tomm00se <tom@moose17.dev>
2024-09-12 14:48:12 +0100

    extrapolated pokemon data array into its own file

---
+++ utils/pokemonData.tsx
@@ -0,0 +1,36 @@
+const pokemonData = [
+  {
+    name: "Bulbasaur",
+    id: "#001",
+    type: "Grass",
+    secondType: "Poison",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+  },
+  {
+    name: "Ivysaur",
+    id: "#002",
+    type: "Grass",
+    secondType: "Poison",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png",
+  },
+  {
+    name: "Venusaur",
+    id: "#003",
+    type: "Grass",
+    secondType: "Poison",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png",
+  },
+  {
+    name: "Charmander",
+    id: "#004",
+    type: "Fire",
+    secondType: "Flying",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
+  },
+];
+
+export default pokemonData;
