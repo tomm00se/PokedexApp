70ecaa9109b0c55bc506bc5e18e9590c078629df
Tommy Bowden <tom@moose17.dev>
2024-09-28 19:20:47 +0100

    update detail header to render header component

--- components/DetailsHeader.tsx
+++ components/DetailsHeader.tsx
@@ -1,12 +1,48 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
 import React from "react";
+import { IPokemonDetails } from "@/types/IPokemonDetails";
+import { getBackgroundColorCode } from "@/utils/getBackgroundColorCode";
 
-const DetailsHeader = () => {
+const DetailsHeader = (pokemonData: IPokemonDetails) => {
+  console.log(pokemonData);
+
   return (
-    <View>
-      <Text>DetailsHeader</Text>
+    <View style={[styles.container]}>
+      <View style={styles.details}>
+        <Text style={styles.title}>{pokemonData.name}</Text>
+        <Text style={styles.id}>{`#${pokemonData.id}`}</Text>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    maxHeight: "50%",
+    width: "100%",
+    borderRadius: 22,
+  },
+  details: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+    padding: 20,
+    textTransform: "capitalize",
+    zIndex: 999999,
+    color: "#ffffff",
+  },
+  id: {
+    fontSize: 32,
+    fontWeight: "bold",
+    padding: 20,
+    textTransform: "capitalize",
+    zIndex: 999999,
+    color: "#ffffff",
+  },
+});
 
 export default DetailsHeader;
