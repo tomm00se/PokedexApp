f919c394394d9e89fd96ad682e41823ae9c74be1
Tommy Bowden <tom@moose17.dev>
2024-09-28 18:22:43 +0100

    uses api call to get data from pokeAPI in state and uses it as data to render on page

--- app/(tabs)/Pokedex/[pokemon].tsx
+++ app/(tabs)/Pokedex/[pokemon].tsx
@@ -1,28 +1,43 @@
-import { View, Text } from "react-native";
-import React, { useEffect } from "react";
+import { View, Text, StyleSheet, SafeAreaView } from "react-native";
+import React, { useEffect, useState } from "react";
 import { useLocalSearchParams } from "expo-router";
 import { getPokemonById } from "@/utils/axios/getPokemonById";
+import { IPokemonDetails } from "@/types/IPokemonDetails";
 
-export default function pokemonDetails() {
+export default function PokemonDetails() {
   const { pokemon: pokemonId } = useLocalSearchParams();
+  const [pokemonData, setPokemonData] = useState<IPokemonDetails | null>(null);
 
-  const fetchData = async () => {
+  const fetchData = async (pokemonId: string) => {
     try {
-      const response = await getPokemonById(pokemonId); //TODO: why is this not a string? how can it be string[]??
-      console.log(response);
-      return response;
+      const response = await getPokemonById(pokemonId); //Typeguard in useEffect
+      setPokemonData(response);
     } catch (error) {
       console.error("Error fetching Pokemon by ID:", error);
     }
   };
 
   useEffect(() => {
-    fetchData();
-  }, []);
+    if (typeof pokemonId === "string") {
+      fetchData(pokemonId);
+    }
+  }, [pokemonId]);
 
   return (
-    <View>
-      <Text>{}</Text>
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      {pokemonData && <Text style={styles.title}>{pokemonData.name}</Text>}
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+    padding: 20,
+    textTransform: "capitalize",
+  },
+});
