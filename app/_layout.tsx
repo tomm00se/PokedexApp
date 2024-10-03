1f7f2ffb56c1f7a27347bf3ceab6fa04709fb13c
tomm00se <tom@moose17.dev>
2024-10-03 13:37:17 +0100

    update app to be single page pokedex app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from "expo-router";
 const RootLayout = () => {
   return (
     <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="Pokedex" options={{ headerShown: false }} />
     </Stack>
   );
 };
