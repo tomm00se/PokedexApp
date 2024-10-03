a3d2f1d663c724c899405e02871ee9fe0260fa82
tomm00se <tom@moose17.dev>
2024-10-03 13:39:02 +0100

    moved out of tabs because the app is only pokdex

---
+++ app/Pokedex/index.tsx
@@ -0,0 +1,104 @@
+import {
+  View,
+  Text,
+  StyleSheet,
+  ListRenderItemInfo,
+  FlatList,
+  SafeAreaView,
+  ActivityIndicator,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import { IPokemonTile } from "@/types/IPokemonTile";
+
+import { getAllPokemon } from "@/utils/axios/getAllPokemon";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { PokemonTile } from "@/components/PokemonTile";
+
+const Pokèdex = () => {
+  const [pokemonData, setPokemonData] = useState<IPokemonTile[]>([]);
+  const [showFooter, setShowFooter] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const fetchData = async () => {
+    try {
+      const paginatedResponse = await getAllPokemon(currentPage);
+      setPokemonData([...pokemonData, ...paginatedResponse]);
+    } catch (error) {
+      console.error("Error fetching Pokemon:", error);
+    } finally {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const renderItem = ({ item }: ListRenderItemInfo<IPokemonTile>) => (
+    <GestureHandlerRootView>
+      <PokemonTile {...item} />
+    </GestureHandlerRootView>
+  );
+
+  const renderFooter = () => {
+    return showFooter ? (
+      <ActivityIndicator animating={true} size="large" color="#000000" />
+    ) : (
+      ""
+    );
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <View>
+        <Text style={styles.title}>Pokèdex</Text>
+        <FlatList
+          style={styles.flatlist}
+          data={pokemonData}
+          renderItem={renderItem}
+          numColumns={2}
+          keyExtractor={(item) => item.id}
+          contentContainerStyle={styles.contentContainerStyle}
+          columnWrapperStyle={styles.columnWrapperStyle}
+          initialNumToRender={10}
+          onEndReached={() => {
+            setShowFooter(true);
+            fetchData();
+          }}
+          onEndReachedThreshold={0.5}
+          ListFooterComponent={renderFooter}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Pokèdex;
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+    padding: 20,
+  },
+  link: {
+    fontSize: 16,
+    paddingHorizontal: 20,
+  },
+  flatlist: {
+    marginBottom: 40,
+  },
+  contentContainerStyle: {
+    justifyContent: "space-around",
+    padding: 20,
+  },
+  columnWrapperStyle: {
+    display: "flex",
+    flexBasis: "50%",
+    marginBottom: 10,
+    gap: 10,
+  },
+});
