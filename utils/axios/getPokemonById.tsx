7308ff80d4428310d5a8aaff6c81cdcc139f2acb
tomm00se <tom@moose17.dev>
2024-10-09 14:45:59 +0100

    updated the async fetch to return a type promise to allow for better navigaton through response

--- utils/axios/getPokemonById.tsx
+++ utils/axios/getPokemonById.tsx
@@ -1,6 +1,7 @@
+import { IPokemonDetails } from "@/types/IPokemonDetails";
 import apiClient from ".";
 
-export const getPokemonById = async (id: string) => {
+export const getPokemonById = async (id: string): Promise<IPokemonDetails> => {
   try {
     const response = await apiClient.get(`pokemon/${id}`);
     return response.data;
